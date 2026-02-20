f7bad3289b0896bd15b301fad0cd7cb61ae5063a
AydinKh <aydinkhalilzadeh35@gmail.com>
2026-02-21 00:59:16 +0330

    added error handling to our python app so when an error accurs when scraping, it's details won't be exposed to our js app

--- accu.py
+++ accu.py
@@ -14,24 +14,27 @@ headers = {
     "Accept-Encoding": "gzip, deflate, br",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8", "Connection": "keep-alive",
 }
-def start(url,url_air_qual):
-    response = requests.get(url, headers=headers)
-    response_air_qual=requests.get(url_air_qual,headers=headers)
-    response.raise_for_status()
-    response_air_qual.raise_for_status()
+try:
+        def start(url,url_air_qual):
+                response = requests.get(url, headers=headers)
+                response_air_qual=requests.get(url_air_qual,headers=headers)
+                response.raise_for_status()
+                response_air_qual.raise_for_status()
 
-    soup = BeautifulSoup(response.text, "html.parser")
-    soup_air_qual=BeautifulSoup(response_air_qual.text,"html.parser")
+                soup = BeautifulSoup(response.text, "html.parser")
+                soup_air_qual=BeautifulSoup(response_air_qual.text,"html.parser")
 
-    temp=soup.find("div",class_='display-temp')
-    detailed_data= soup.find("div", class_="detail-item spaced-content")
-    city= soup.find("h1", class_="header-loc")
-    phrase=soup.find("div", class_='phrase')
-    air_qual=soup_air_qual.find("div", class_='aq-number')
-    print("\nCity:", city.get_text(strip=True) if city else "Not found")
-    print('\nTemperature:',temp.get_text(strip=True)if temp else "Not found")
-    print(f"\nWeather condition: {phrase.get_text(strip=True) if phrase else 'Not found'}")
-    print(f"\nAir quality: {air_qual.get_text(strip=True) if air_qual else 'Not found'} AQI")
-    print(detailed_data.parent.get_text() if detailed_data else "Not found")
+                temp=soup.find("div",class_='display-temp')
+                detailed_data= soup.find("div", class_="detail-item spaced-content")
+                city= soup.find("h1", class_="header-loc")
+                phrase=soup.find("div", class_='phrase')
+                air_qual=soup_air_qual.find("div", class_='aq-number')
+                print("\nCity:", city.get_text(strip=True) if city else "Not found")
+                print('\nTemperature:',temp.get_text(strip=True)if temp else "Not found")
+                print(f"\nWeather condition: {phrase.get_text(strip=True) if phrase else 'Not found'}")
+                print(f"\nAir quality: {air_qual.get_text(strip=True) if air_qual else 'Not found'} AQI")
+                print(detailed_data.parent.get_text() if detailed_data else "Not found")
+        
+except ValueError:
+            print(error)
 start(url,url_air_qual)
-
